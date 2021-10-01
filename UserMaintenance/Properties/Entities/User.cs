5bd91a318694b8ffe39b9e3201c9660f62852702
Milán Péter Novák <milan.novak@stud.uni-corvinus.hu>
2021-10-01 19:24:52 +0200

    FullName commit

--- UserMaintenance/Properties/Entities/User.cs
+++ UserMaintenance/Properties/Entities/User.cs
@@ -3,16 +3,14 @@
 public class User
 {
     public Guid ID { get; set; } = Guid.NewGuid();
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string FullName
+    public string FullName { get; set; };
     {
         get
         {
             return string.Format(
                 "{0} {1}",
-                LastName,
-                FirstName);
+                FullName,
+                );
         }
     }
 
